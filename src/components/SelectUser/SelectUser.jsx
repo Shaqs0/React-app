30bbd6eac370977a3eb69039776e9f074e5a03a6
Shaqs0 <hhgfhhhggb@gmail.com>
2024-02-14 13:47:12 +0300

    correct

--- src/components/SelectUser/SelectUser.jsx
+++ src/components/SelectUser/SelectUser.jsx
@@ -1,14 +1,10 @@
 import styles from './SelectUser.module.css';
 
-function SelectUser({ changedUser }) {
-	const changeUser = (e) => {
-		changedUser(e.target.value);
-		console.log(e.target.value);
-	};
+function SelectUser() {
 	return (
 		<>
 			<img className={styles.logo} src="/logo.svg" alt="Логотип журнала" />
-			<select name="user" id="user" onChange={changeUser}>
+			<select name="user" id="user">
 				<option value='1'>Антон</option>
 				<option value='2'>Вася</option>
 			</select>
