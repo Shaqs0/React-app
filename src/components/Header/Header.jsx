2c81619ef151e6dda46d2d515eeac166b141e667
Shaqs0 <hhgfhhhggb@gmail.com>
2024-02-14 13:00:33 +0300

    /

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import SelectUser from '../SelectUser/SelectUser';
+import styles from './Header.module.css';
+
+function Header({ changedUser }) {
+	const changeUser = (e) => {
+		changedUser(e.target.value);
+		console.log(e.target.value);
+	};
+	return (
+		<>
+			<img className={styles.logo} src="/logo.svg" alt="Логотип журнала" />
+			<SelectUser changedUser={changeUser}/>
+		</>
+	);
+}
+
+export default Header;
---
+++ src/components/JournalForm/JournalForm.jsx
@@ -0,0 +1,90 @@
+import styles from './JournalForm.module.css';
+import Button from '../Button/Button';
+import { useEffect, useReducer, useRef} from 'react';
+import cn from 'classnames';
+import { INITIAL_STATE, formReducer } from './JournalForm.state';
+import Input from '../Input/Input';
+
+
+
+function JournalForm ({onSubmit}) {
+	const [formState, dispatchForm] = useReducer(formReducer, INITIAL_STATE);
+	const { isValid, isFormReadyToSubmit, values } = formState; 
+	const titleRef = useRef();
+	const dateRef = useRef();
+	const postRef = useRef();
+
+	const focusError = (isValid) => {
+		switch(true) {
+		case !isValid.title:
+			titleRef.current.focus();
+			break;
+		case !isValid.date:
+			dateRef.current.focus();
+			break;
+		case !isValid.post:
+			postRef.current.focus();
+			break;			
+		}
+	};
+
+	useEffect(() => {
+		let timerId;
+		if (!isValid.date || !isValid.post || !isValid.title) {
+			focusError(isValid);
+			timerId = setTimeout(() => {
+				dispatchForm({ type: 'RESET_VALIDITY' });
+			}, 2000);
+		}
+		return () => {
+			clearTimeout(timerId);
+		};
+	}, [isValid]);
+
+
+	useEffect(() => {
+		if (isFormReadyToSubmit) {
+			onSubmit(values);
+			dispatchForm({ type: 'CLEAR' });
+		}
+	}, [isFormReadyToSubmit, values, onSubmit]);
+
+	const onChange = (e) => {
+		dispatchForm({ type: 'SET_VALUE', payload: { [e.target.name]: e.target.value } });
+	};
+
+	const addJournalItem = (e) => {
+		e.preventDefault();
+		dispatchForm({ type: 'SUBMIT'});
+	};
+	
+
+	return (
+		<form className={styles['journal-form']} onSubmit={addJournalItem}>
+			<div>
+				<Input type='text' ref={titleRef} inValid={isValid.title} onChange={onChange} value={values.title} name='title' appearance='title'/>
+			</div>
+			<div className={styles['form-row']}>
+				<label htmlFor="date"  className={styles['form-label']}>
+					<img src='/calendar.svg' alt='Иконка календаря'/>
+					<span>Дата</span>
+				</label>
+
+				<Input type='date' ref={dateRef} inValid={isValid.date} onChange={onChange} value={values.date} name='date' id="date"/>
+			</div>
+			<div className={styles['form-row']}>
+				<label htmlFor="tag" className={styles['form-label']}>
+					<img src='/folder.svg' alt='Иконка папки'/>
+					<span>Метки</span>
+				</label>
+				<Input type='text' onChange={onChange} id="tag" value={values.tag} name='tag'/>
+			</div>
+			<textarea ref={postRef} name='post' id='' onChange={onChange} value={values.post} cols="30" rows="10" className={cn(styles['input'], {
+				[styles['invalid']] : !isValid.post
+			})}></textarea>
+			<Button text='Сохранить' />
+		</form>
+	);
+}
+
+export default JournalForm;
